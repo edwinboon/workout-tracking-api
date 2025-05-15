9ab53ac30ca93365ea1f1bac7804b90d5fc4ce01
Edwin Boon <hello@edwinboon.dev>
2025-05-15 20:21:43 +0200

    Setup user_store

---
+++ internal/store/user_store.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+)
+
+type User struct {
+	ID           int64     `json:"id"`
+	Username     string    `json:"username"`
+	Email        string    `json:"email"`
+	PasswordHash string    `json:"-"` // Don't include password hash in JSON response
+	Bio          string    `json:"bio"`
+	AvatarURL    string    `json:"avatar_url"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+}
+
+type PostgresUserStore struct {
+	db *sql.DB
+}
+
+func NewPostgresUserStore(db *sql.DB) *PostgresUserStore {
+	return &PostgresUserStore{
+		db: db,
+	}
+}
